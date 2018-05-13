59946b4d49c883e3d4e0750f5ab6545691cc3982
Ovidiu Cherecheș <skidding@gmail.com>
2018-05-13 22:05:29 +0700

    Add files

---
+++ cosmos.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  containerQuerySelector: '#root',
+  webpackConfigPath: 'react-scripts/config/webpack.config.dev',
+  publicPath: 'public',
+  // Optional: Create this file when you begin adding proxies
+  proxiesPath: 'src/cosmos.proxies'
+};
