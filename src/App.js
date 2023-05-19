3e1dc7553b29303f8ab9d1d649594da8f5af4c01
Ovidiu Cherecheș <hello@ovidiu.ch>
2023-05-19 14:52:19 +0300

    Update stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          123123
+          Learn React
         </a>
       </header>
     </div>
